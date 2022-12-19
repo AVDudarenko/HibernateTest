89cb92b8b6099729eecd996af01a7346efe827dd
AVDudarenko <aleksandr.dudareko@gmail.com>
2022-12-20 00:29:24 +0300

    commit

--- src/main/java/org/example/HibernateSessionFactoryUtil.java
+++ src/main/java/org/example/HibernateSessionFactoryUtil.java
@@ -5,51 +5,32 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HibernateSessionFactoryUtil {
-    private static StandardServiceRegistry registry;
-    private static SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
 
-                Map<String, String> settings = new HashMap<>();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "26061995");
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
 
-                registryBuilder.applySettings(settings);
-
-                registry = registryBuilder.build();
-
-                MetadataSources sources = new MetadataSources(registry);
-                sources.addAnnotatedClass(PetsInfo.class);
-                Metadata metadata = sources.getMetadataBuilder().build();
-
-                sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                if (registry != null) {
-                    StandardServiceRegistryBuilder.destroy(registry);
-                }
-            }
+            return configuration
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(
+                    "There was an error building the factory");
         }
-        return sessionFactory;
     }
 
-    public static void close() {
-        if (registry != null) {
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
+        PetsDAO petsDAO = new PetsDAO();
+
+        PetsInfo barsik = new PetsInfo("barsik", 12, "white", "barmaley");
+        petsDAO.save(barsik);
+
         System.out.println("Hello world!");
     }
 }
